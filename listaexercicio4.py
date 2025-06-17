df1cca1fef5226489494688c8b16010a6789c3e3
Lucasrombach <lucasrombachdacruz@gmail.com>
2025-06-17 10:50:56 -0300

    Update listaexercicio4.py

--- listaexercicio4.py
+++ listaexercicio4.py
@@ -45,6 +45,8 @@ st.dataframe(df.head(len(df)))
 - ROA = Lucro Líquido / Ativo Total *  100
 """
 
+import matplotib.pyplot as plt
+
 df["Margem Líquida"] = df["Lucro Líquido"] / df["Receita Líquida"] * 100
 df["ROA"] = df["Lucro Líquido"] / df["Ativo Total"] * 100
 
